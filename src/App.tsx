0592fc28fcd04b6b8d58a8806064defdc2204872
Uzay Poyraz <upoyrazlol@gmail.com>
2025-08-10 04:03:48 -0400

    added highlight to the subpage you are on

--- src/App.tsx
+++ src/App.tsx
@@ -52,12 +52,27 @@ function AppContent() {
             <Link to="/">LaunchSpace</Link>
           </div>
           
-          {/* Desktop Navigation */}
-          <div className="nav-links desktop-nav">
-            <Link to="/apps">Apps</Link>
-            <Link to="/career">Careers</Link>
-            <Link to="/contact">Contact</Link>
-          </div>
+                      {/* Desktop Navigation */}
+            <div className="nav-links desktop-nav">
+              <Link 
+                to="/apps" 
+                className={location.pathname === '/apps' ? 'active' : ''}
+              >
+                Apps
+              </Link>
+              <Link 
+                to="/career" 
+                className={location.pathname === '/career' ? 'active' : ''}
+              >
+                Careers
+              </Link>
+              <Link 
+                to="/contact" 
+                className={location.pathname === '/contact' ? 'active' : ''}
+              >
+                Contact
+              </Link>
+            </div>
 
           {/* Mobile Hamburger Menu */}
           <div className="hamburger-menu">
@@ -73,31 +88,43 @@ function AppContent() {
           </div>
         </div>
 
-        {/* Mobile Navigation Menu */}
-        <div className={`mobile-nav ${isMenuOpen ? 'active' : ''}`}>
-          <Link to="/apps" onClick={() => setIsMenuOpen(false)}>
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="2" y="3" width="20" height="14" rx="2" ry="2" stroke="currentColor" strokeWidth="2" fill="none"/>
-              <line x1="8" y1="21" x2="16" y2="21" stroke="currentColor" strokeWidth="2"/>
-              <line x1="12" y1="17" x2="12" y2="21" stroke="currentColor" strokeWidth="2"/>
-            </svg>
-            Apps
-          </Link>
-          <Link to="/career" onClick={() => setIsMenuOpen(false)}>
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M20 7L10 17L5 12" stroke="currentColor" strokeWidth="2" fill="none"/>
-              <path d="M21 12V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H12" stroke="currentColor" strokeWidth="2" fill="none"/>
-            </svg>
-            Careers
-          </Link>
-          <Link to="/contact" onClick={() => setIsMenuOpen(false)}>
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="currentColor" strokeWidth="2" fill="none"/>
-              <polyline points="22,6 12,13 2,6" stroke="currentColor" strokeWidth="2" fill="none"/>
-            </svg>
-            Contact
-          </Link>
-        </div>
+                  {/* Mobile Navigation Menu */}
+          <div className={`mobile-nav ${isMenuOpen ? 'active' : ''}`}>
+            <Link 
+              to="/apps" 
+              onClick={() => setIsMenuOpen(false)}
+              className={location.pathname === '/apps' ? 'active' : ''}
+            >
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="2" y="3" width="20" height="14" rx="2" ry="2" stroke="currentColor" strokeWidth="2" fill="none"/>
+                <line x1="8" y1="21" x2="16" y2="21" stroke="currentColor" strokeWidth="2"/>
+                <line x1="12" y1="17" x2="12" y2="21" stroke="currentColor" strokeWidth="2"/>
+              </svg>
+              Apps
+            </Link>
+            <Link 
+              to="/career" 
+              onClick={() => setIsMenuOpen(false)}
+              className={location.pathname === '/career' ? 'active' : ''}
+            >
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 7L10 17L5 12" stroke="currentColor" strokeWidth="2" fill="none"/>
+                <path d="M21 12V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H12" stroke="currentColor" strokeWidth="2" fill="none"/>
+              </svg>
+              Careers
+            </Link>
+            <Link 
+              to="/contact" 
+              onClick={() => setIsMenuOpen(false)}
+              className={location.pathname === '/contact' ? 'active' : ''}
+            >
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="currentColor" strokeWidth="2" fill="none"/>
+                <polyline points="22,6 12,13 2,6" stroke="currentColor" strokeWidth="2" fill="none"/>
+              </svg>
+              Contact
+            </Link>
+          </div>
       </nav>
 
       {/* Routes */}
